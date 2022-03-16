4195d1ca6c06da5c86c485bae1b507ef2123465e
alepiazza <alepiazza@live.it>
2022-03-16 15:26:33 +0100

    Tests: testing `SDP` with `name` argument

--- tests/test_sdp.py
+++ tests/test_sdp.py
@@ -10,7 +10,7 @@ def have_binary(bin_name):
     return bin_in_path is not None and os.path.isfile(bin_in_path)
 
 
-class TestCBTSeed1(unittest.TestCase):
+class TestSDP(unittest.TestCase):
     def setUp(self):
         table1 = ConformalBlockTable(3, 10, 10, 2, 4)
         table2 = ConvolvedBlockTable(table1)
@@ -27,10 +27,12 @@ class TestCBTSeed1(unittest.TestCase):
 
         self.sdp = SDP(0.518, table2, sdpb_mode=sdpb_mode, sdpb_kwargs=sdpb_kwargs)
 
+    def tearDown(self):
+        shutil.rmtree("test_output")
+
     def test_sdp_bisect(self):
         self.sdp.sdpb.set_option("procsPerNode", 1)
-        self.sdp.sdpb.set_option("sdpDir", "test_output/test")
-        self.sdp.bisect(0.7, 1.7, 0.05, 0)
+        self.sdp.bisect(0.7, 1.7, 0.1, 0, name="test_output/test_bisect")
 
 
 if __name__ == '__main__':
