63511159be126a38a5144ce725cc036a064bc47a
alepiazza <alepiazza@live.it>
2022-05-09 16:30:31 +0200

    Example: O(N) bugfix

--- examples/ON_3d.py
+++ examples/ON_3d.py
@@ -15,11 +15,11 @@ tableG = ConformalBlockTable(dim, k_max, l_max, m_max, n_max, odd_spins=True)
 tableFplus = ConvolvedBlockTable(tableG, symmetric=True)
 tableFminus = ConvolvedBlockTable(tableG)
 
-N = 10
-tables = [tableFminus, tableFplus]
-vecS = [[0, 0], [1, 0], [1, 1]]
-vecT = [[1, 0], [1 - 2 / N, 0], [-1 + 2 / N, 1]]
-vecA = [[1, 0], [-1, 0], [1, 1]]
+N = 3
+tables = [tableFplus, tableFminus]
+vecS = [[0, 1], [1, 1], [1, 0]]
+vecT = [[1, 1], [1 - (2 / N), 1], [-(1 + (2 / N)), 0]]
+vecA = [[1, 1], [-1, 1], [1, 0]]
 vector_types = [[vecS, 0, 'singlet'], [vecT, 0, 'symmetric'], [vecA, 1, 'antisymmetric']]
 
 dim_phi = np.linspace(0.515, 0.7, 5)
@@ -27,14 +27,14 @@ dim_eps = []
 
 for phi in dim_phi:
     sdp = SDP(phi, tables, vector_types=vector_types, sdpb_mode='docker')
-    sdp.sdpb.set_option("procsPerNode", 2)
+    sdp.sdpb.set_option('procsPerNode', 2)
 
     lower = 0.5
     upper = 2
     tol = 0.001
     channel = [0, 'singlet']
 
-    result = sdp.bisect(lower, upper, tol, channel, name=f"work/ON_{N}_{phi}")
+    result = sdp.bisect(lower, upper, tol, channel, name=f'work/ON_{N}_{phi}')
     dim_eps.append(result)
 
 np.savetxt(f'ON_{N}.txt', np.array(list(zip(dim_phi, dim_eps))))
