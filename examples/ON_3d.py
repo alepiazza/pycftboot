c300d4e7796a5c3112213365738abde6608f8183
alepiazza <alepiazza@live.it>
2022-06-06 12:45:30 +0200

    Examples: fixed ON crossing

--- examples/ON_3d.py
+++ examples/ON_3d.py
@@ -17,27 +17,27 @@ tableFminus = ConvolvedBlockTable(tableG)
 
 N = 3
 tables = [tableFplus, tableFminus]
-vecS = [[1, 1], [0, 0], [1, 0]]
-vecT = [[N - 1, 1], [1, 1], [-(1 + N / 2), 0]]
-vecA = [[0, 0], [1, 1], [N / 2, 0]]
+vecS = [[0, 1], [1, 1], [1, 0]]
+vecT = [[1, 1], [1 - (2 / N), 1], [-(1 + (2 / N)), 0]]
+vecA = [[1, 1], [-1, 1], [1, 0]]
 vector_types = [[vecS, 0, 'singlet'], [vecT, 0, 'symmetric'], [vecA, 1, 'antisymmetric']]
 
-dim_phi = np.linspace(0.52, 0.53, 1)
+dim_phi = np.linspace(0.5000001, 0.6, 20)
 dim_eps = []
 
 for phi in dim_phi:
-    sdp = SDP(phi, tables, vector_types=vector_types, sdpb_mode='docker')
+    sdp = SDP(phi, tables, vector_types=vector_types, sdpb_mode='binary')
     sdp.sdpb.set_option('procsPerNode', 2)
 
     lower = 0.5
     upper = 4
-    tol = 0.01
+    tol = 0.001
     channel = [0, 'singlet']
 
-    result = sdp.bisect(lower, upper, tol, channel, name=f'work/ON_{N}_{phi}')
+    result = sdp.bisect(lower, upper, tol, channel, name=f'work/ON_3d_{N}_{phi}')
     dim_eps.append(result)
+    print(f"Bound at (dim_phi, dim_eps) = ({phi}, {result})")
 
-np.savetxt(f'ON_{N}.txt', np.array(list(zip(dim_phi, dim_eps))))
-
+np.savetxt(f'ON_3d_{N}.txt', np.array(list(zip(dim_phi, dim_eps))))
 plt.plot(dim_phi, dim_eps)
-plt.savefig(f'ON_{N}.png', dpi=600)
+plt.savefig(f'ON_3d_{N}.png', dpi=600)
